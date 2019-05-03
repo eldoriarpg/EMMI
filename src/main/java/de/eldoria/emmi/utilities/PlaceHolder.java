df63455c78769ab2f49cc56ea3d1bcf18791c7e9
RainbowDashLabs <mail@code-fox.de>
2019-05-03 14:34:39 +0200

    add placerholder creator

--- src/main/java/de/eldoria/emmi/utilities/PlaceHolder.java
+++ src/main/java/de/eldoria/emmi/utilities/PlaceHolder.java
@@ -1,5 +1,7 @@
 package de.eldoria.emmi.utilities;
 
+import java.util.HashMap;
+
 public class PlaceHolder {
     private String placeholder;
     private String replacement;
@@ -11,7 +13,7 @@ public class PlaceHolder {
      * @param placeholder the placeholder
      * @param replacement value with which the placeholder should be replaced.
      */
-    public PlaceHolder(String placeholder, String replacement) {
+    private PlaceHolder(String placeholder, String replacement) {
         this.placeholder = placeholder;
         this.replacement = replacement;
     }
@@ -24,5 +26,52 @@ public class PlaceHolder {
      */
     public String apply(String s) {
         return s.replace(placeholder, replacement);
+    }
+
+
+    public static class Creator {
+
+        private HashMap<String, String> placeholderHashMap = new HashMap<>();
+
+        private PlaceHolder[] placeholder;
+        private boolean changed = true;
+
+        public PlaceHolder create(String placeholder, String value){
+            PlaceHolder newPlaceholder = new PlaceHolder(placeholder, value);
+            placeholderHashMap.put(placeholder,value);
+            changed = true;
+            return newPlaceholder;
+        }
+
+        public void changePlaceholderValue(String key, String value) {
+            if (placeholderHashMap.containsKey(key)) {
+                placeholderHashMap.replace(key, value);
+            }
+            changed = true;
+        }
+
+        public void removePlaceholder(String key){
+            placeholderHashMap.remove(key);
+            changed = true;
+        }
+
+        public PlaceHolder[] getPlaceholder(){
+            if(changed){
+                placeholder = new PlaceHolder[placeholderHashMap.size()];
+                int i = 0;
+                for (String key : placeholderHashMap.keySet()){
+                    placeholder[i] = new PlaceHolder(key, placeholderHashMap.get(key));
+                    i++;
+                }
+                changed = false;
+            }
+            return placeholder;
+        }
+
+        public void clearPlaceholder(){
+            placeholderHashMap.clear();
+            placeholder = new PlaceHolder[0];
+            changed = true;
+        }
     }
 }
